b8a0d63ed5311e366f67bfcc6197c5e51881a627
Juzo <fqqwsj@gmail.com>
2022-05-24 13:39:19 +0300

    full desktop

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,16 @@ $(function () {
     slidesToScroll: 10,
   });
 
+  $('.blog__item-slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="blog__item-arrow blog__item-arrow-left"><img src="images/slide-arrow-left.svg" alt="previous"></button>',
+    nextArrow: '<button type="button" class="blog__item-arrow blog__item-arrow-right"><img src="images/slide-arrow-right.svg" alt="next"></button>',
+  
+  });
+
   var mixer = mixitup('.gallery__main', {
     load: {
       filter: '.living'
